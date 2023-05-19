bb006c9d37f695caf34031489a175e546d17ea4b
joi0104 <jjjjoi0104@gmail.com>
2023-05-19 21:35:16 +0900

    fix: deploy.sh추가

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+/usr/local/bin/docker-compose -f ~/rms-web-server/docker-compose.yml down
+/usr/local/bin/docker pull jjjjoi0104/rms-web-server:latest
+/usr/local/bin/docker-compose -f ~/rms-web-server/docker-compose.yml up -d
