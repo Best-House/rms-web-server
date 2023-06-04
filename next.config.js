6b21ecdc96ae08fb7d8eb32b39f4380e5f6fcac5
joi0104 <jjjjoi0104@gmail.com>
2023-06-04 16:21:35 +0900

    feat: path rewrite 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: { appDir: true },
   output: "standalone",
+  async rewrites() {
+		return [
+			{
+				source: "/api/:path",
+				destination: "https://hojun.asuscomm.com/api/:path",
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
