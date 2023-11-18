74f1038842f63b91c16ff3f5c70ba5ce57513ffe
joi0104 <jjjjoi0104@gmail.com>
2023-11-18 21:07:13 +0900

    fix: reactStrictMode false 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: false },
+  reactStrictMode: false,
   output: "standalone",
   async rewrites() {
     return [
