42eb30e8fc0b672a9288f0f2fd9c5d0e824d6bd1
Jisoo Youn <yjs930915@gmail.com>
2023-12-02 00:53:22 +0900

    refactor test

--- src/domain/model/material/MaterialService.spec.ts
+++ src/domain/model/material/MaterialService.spec.ts
@@ -1,21 +1,20 @@
 import { test, expect, describe, vi } from "vitest";
-import { Material } from "@/domain/model/material/Material";
+import { DraftMaterial, Material } from "@/domain/model/material/Material";
 import { MaterialService } from "@/domain/model/material/MaterialService";
 
 describe("원재자", () => {
   test("원자재들을 조회한다.", async () => {
-    const mockMaterialRepository = {
-      findAllMaterials(): Promise<Material[]> {
-        return Promise.resolve([
-          new Material("1", "테스트원자재", 10000),
-          new Material("2", "테스트원자재2", 20000),
-        ]);
-      },
-      saveMaterial(_): Promise<Material> {
-        return Promise.reject(_);
-      },
-      removeMaterial: vi.fn(),
-    };
+    const mockMaterialRepository = new MockMaterialRepositoryBuilder()
+      .onFindAllMaterials(
+        vi.fn(() =>
+          Promise.resolve([
+            new Material("1", "테스트원자재", 10000),
+            new Material("2", "테스트원자재2", 20000),
+          ]),
+        ),
+      )
+      .build();
+
     const materialService = new MaterialService(mockMaterialRepository);
     const materials = await materialService.getMaterials();
     expect(materials).toStrictEqual([
@@ -25,18 +24,17 @@ describe("원재자", () => {
   });
 
   test("원자재를 생성한다", async () => {
-    const materialRepository = {
-      findAllMaterials(): Promise<Material[]> {
-        return Promise.reject();
-      },
-      saveMaterial(params): Promise<Material> {
-        return Promise.resolve(
-          new Material("testId", params.name, params.defaultUnitPrice),
-        );
-      },
-      removeMaterial: vi.fn(),
-    };
-    const materialService = new MaterialService(materialRepository);
+    const mockMaterialRepository = new MockMaterialRepositoryBuilder()
+      .onSaveMaterial(
+        vi.fn((params) =>
+          Promise.resolve(
+            new Material("testId", params.name, params.defaultUnitPrice),
+          ),
+        ),
+      )
+      .build();
+
+    const materialService = new MaterialService(mockMaterialRepository);
     const createdMaterial = await materialService.createMaterial({
       name: "테스트원자재",
       defaultUnitPrice: 10000,
@@ -47,20 +45,10 @@ describe("원재자", () => {
   });
 
   test("원자재를 삭제한다", async () => {
-    const mockMaterialRepository = {
-      findAllMaterials: vi.fn(() =>
-        Promise.resolve([
-          new Material("1", "테스트원자재", 10000),
-          new Material("2", "테스트원자재2", 20000),
-        ]),
-      ),
-      saveMaterial: vi.fn((params) =>
-        Promise.resolve(
-          new Material("testId", params.name, params.defaultUnitPrice),
-        ),
-      ),
-      removeMaterial: vi.fn((params) => Promise.resolve(params)),
-    };
+    const mockMaterialRepository = new MockMaterialRepositoryBuilder()
+      .onRemoveMaterial(vi.fn((params) => Promise.resolve(params)))
+      .build();
+
     const materialService = new MaterialService(mockMaterialRepository);
     const mockMaterial = new Material("1", "테스트원자재", 10000);
 
@@ -68,3 +56,36 @@ describe("원재자", () => {
     expect(mockMaterialRepository.removeMaterial).toBeCalledWith(mockMaterial);
   });
 });
+
+class MockMaterialRepositoryBuilder {
+  private findAllMaterials: () => Promise<Material[]>;
+  private saveMaterial: (draftMaterial: DraftMaterial) => Promise<Material>;
+  private removeMaterial: (material: Material) => Promise<Material>;
+  constructor() {
+    this.findAllMaterials = vi.fn();
+    this.saveMaterial = vi.fn();
+    this.removeMaterial = vi.fn();
+  }
+
+  onFindAllMaterials(fn: () => Promise<Material[]>) {
+    this.findAllMaterials = fn;
+    return this;
+  }
+
+  onSaveMaterial(fn: (draftMaterial: DraftMaterial) => Promise<Material>) {
+    this.saveMaterial = fn;
+    return this;
+  }
+
+  onRemoveMaterial(fn: (material: Material) => Promise<Material>) {
+    this.removeMaterial = fn;
+    return this;
+  }
+  build() {
+    return {
+      findAllMaterials: this.findAllMaterials,
+      saveMaterial: this.saveMaterial,
+      removeMaterial: this.removeMaterial,
+    };
+  }
+}
