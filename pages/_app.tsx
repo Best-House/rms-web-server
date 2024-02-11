70e940bad2a1e36db9da85163e310c2223a86045
joi0104 <jjjjoi0104@gmail.com>
2024-02-11 16:56:17 +0900

    refactor: 더이상 사용하지 않는 코드 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalErrorBoundary } from "@/utils/GlobalErrorBoundary";
-import { ApiClientProvider } from "@/remotes/hooks/useApiClient";
-import { httpApiClient } from "@/remotes/https/HttpApiClient";
 
 const queryClient = new QueryClient();
 
@@ -11,9 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalErrorBoundary>
-        <ApiClientProvider value={httpApiClient}>
-          <Component {...pageProps} />
-        </ApiClientProvider>
+        <Component {...pageProps} />
       </GlobalErrorBoundary>
     </QueryClientProvider>
   );
